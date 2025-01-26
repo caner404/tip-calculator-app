1ff5bfc7e07a115199da7e54c452ab91492079ae
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2025-01-26 21:51:42 +0100

    Enhance input validation and styling in Calculator component

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -28,18 +28,26 @@ export function Calculator() {
     <div className=' bg-white py-8 sm:py-11 px-6 sm:px-0 sm:pl-12 sm:pr-8 rounded-tr-3xl rounded-tl-2xl sm:rounded-3xl flex flex-col sm:flex-row gap-5 max-w-full sm:max-w-[920px] h-full sm:h-fit'>
       <form className='flex flex-col gap-8 sm:gap-10 flex-1 px-2'>
         <div className='flex flex-col gap-2 text-left'>
-          <label
-            htmlFor='billInput'
-            className='text-[#5E7A7D] font-bold text-sm'
-          >
-            Bill
-          </label>
+          <div className='flex justify-between items-center'>
+            <label
+              htmlFor='billInput'
+              className='text-[#5E7A7D] font-bold text-sm'
+            >
+              Bill
+            </label>
+            {bill <= 0 && <p className='text-[#E17457] font-bold text-base'>Can't be smaller then zero</p>}
+          </div>
+
           <input
             id='billInput'
             type='number'
             aria-label='Bill'
             value={bill}
-            className='bg-[#F3F9FA] py-2 px-4 text-[#00474B] font-bold text-2xl text-right rounded-sm'
+            className={`bg-[#F3F9FA] py-2 p-4 text-[#00474B] font-bold text-2xl text-right rounded-sm border-2 focus:outline-none hover:cursor-pointer ${
+              bill <= 0
+                ? 'border-[#E17457] focus:border-[#E17457] hover:border-[#E17457]'
+                : 'border-transparent focus:border-[#26C2AE] hover:border-[#26C2AE] '
+            }`}
             onChange={(e) => {
               setBill(Number(e.target.value));
             }}
@@ -57,7 +65,7 @@ export function Calculator() {
                   key={percentage}
                   id={`${percentage}%-tip`}
                   aria-checked={checked === percentage}
-                  className={`flex items-center justify-center basis-[calc(50%-8px)] sm:w-[110px] h-12 font-bold text-2xl rounded-md hover:cursor-pointer ${
+                  className={`flex items-center justify-center basis-[calc(50%-8px)] sm:w-[110px] h-12 font-bold text-2xl rounded-md hover:cursor-pointer hover:bg-[#9FE8DF] ${
                     checked === percentage ? 'active bg-[#26C2AE] text-[#00474B]' : 'bg-[#00474B] text-white'
                   }`}
                   onClick={() => {
@@ -94,18 +102,24 @@ export function Calculator() {
         </div>
 
         <div className='flex flex-col gap-2 text-left'>
-          <label
-            htmlFor='numberOfPeople'
-            className='text-[#5E7A7D] font-bold text-sm rounded-sm'
-          >
-            Number of People
-          </label>
-          {numberOfPeople === 0 && <p>Can't be zero</p>}
+          <div className='flex justify-between items-center'>
+            <label
+              htmlFor='numberOfPeople'
+              className='text-[#5E7A7D] font-bold text-sm rounded-sm'
+            >
+              Number of People
+            </label>
+            {numberOfPeople <= 0 && <p className='text-[#E17457] font-bold text-base'>Can't be smaller then zero</p>}
+          </div>
           <input
             id='numberOfPeople'
             type='number'
             aria-label='Number of People'
-            className='bg-[#F3F9FA] py-2 p-4 text-[#00474B] font-bold text-2xl text-right'
+            className={`bg-[#F3F9FA] py-2 p-4 text-[#00474B] font-bold text-2xl text-right rounded-sm border-2 focus:outline-none hover:cursor-pointer ${
+              numberOfPeople <= 0
+                ? 'border-[#E17457] focus:border-[#E17457] hover:border-[#E17457]'
+                : 'border-transparent focus:border-[#26C2AE] hover:border-[#26C2AE] '
+            }`}
             value={numberOfPeople}
             onChange={(e) => {
               setNumberOfPeople(Number(e.target.value));
@@ -137,7 +151,7 @@ export function Calculator() {
         </div>
 
         <button
-          className='mt-auto bg-[#26C2AE] rounded-md font-bold text-[#00474B] text-[20px] uppercase py-2 hover:cursor-pointer'
+          className='mt-auto bg-[#26C2AE] rounded-md font-bold text-[#00474B] text-[20px] uppercase py-2 hover:cursor-pointer hover:bg-[#9FE8DF]'
           onClick={resetValues}
         >
           Reset
